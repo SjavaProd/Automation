41301782d43ad11b43f8f76eb0d86d822523d0a3
Vyacheslav Tkalenko <tkalenkovyacheslav@gmail.com>
2019-12-01 01:19:30 +0300

    Homework#3 Task2 is modified

--- src/main/java/com/homework3/Task2v2.java
+++ src/main/java/com/homework3/Task2v2.java
@@ -1,24 +1,36 @@
+/*
+Написать метод который принимает в себя строку в которой есть double значение
+*(например “Get a modern feel  with a smudge-resistant only for $2.50, you can finish now”)
+достаёт из него числовое значение и возвращает как double. После чего этот дабл вызывается
+из другого метода в котором он умножается на 4 и выводится в консоль вместе с текстом
+ */
+
 package com.homework3;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Task2v2 {
     public static void main(String[] args) {
         String sentence = getSentence();
         double price = getPrice(sentence);
         double newPrice = calculate(price);
-        String firstStr = sentence.substring(0, (sentence.indexOf('$') + 1));
-        String secondStr = sentence.substring(sentence.indexOf(','));
-        System.out.println("New string is:\n" + firstStr + newPrice + secondStr);
+        System.out.println("Value of our price is: " + price);
+        System.out.println("String with new price is:\n" + sentence.replaceAll(("\\d.+\\d"), String.valueOf(newPrice)));
     }
 
     public static String getSentence() {
-        String input = "Get a modern feel  with a smudge-resistant only for $2.50, you can finish now";
+        String input = "Get a modern feel  with a smudge-resistant 2.50 only for you can finish now";
         return input;
     }
 
     public static double getPrice(String input) {
-        int index1 = input.indexOf('$');
-        int index2 = input.indexOf(',');
-        double price = Double.valueOf(input.substring(index1 + 1, index2));
+        Pattern pattern = Pattern.compile("\\d.+\\d");
+        Matcher matcher = pattern.matcher(input);
+        double price = 0;
+        while (matcher.find()) {
+            price = Double.parseDouble(input.substring(matcher.start(), matcher.end()));
+        }
         return price;
     }
 
