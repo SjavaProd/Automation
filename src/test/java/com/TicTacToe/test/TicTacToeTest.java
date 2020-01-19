1e5e031daa78bfeb1fa760ac935e3419e051c9f5
Vyacheslav Tkalenko <tkalenkovyacheslav@gmail.com>
2020-01-20 00:17:20 +0200

    Tests with assertions2

--- src/test/java/com/TicTacToe/test/TicTacToeTest.java
+++ src/test/java/com/TicTacToe/test/TicTacToeTest.java
@@ -1,6 +1,7 @@
 package com.TicTacToe.test;
 
 import com.TicTacToe.GameScheme;
+import com.TicTacToe.Status;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -24,43 +25,43 @@ public class TicTacToeTest extends GameScheme {
     @Test
     public void getInvalidInput() {
         System.out.println(setStatus(INVALID_INPUT));
-        Assert.assertEquals(1, getNumber(INVALID_INPUT));
+        Assert.assertEquals(Status.FIRST.getStatusValue(), setStatus(INVALID_INPUT));
     }
 
     @Test
     public void getIncorrectStatus() {
         System.out.println(setStatus(INCORRECT_STATUS));
-        Assert.assertEquals(2, getNumber(INCORRECT_STATUS));
+        Assert.assertEquals(Status.SECOND.getStatusValue(), setStatus(INCORRECT_STATUS));
     }
 
     @Test
     public void getDraw() {
         System.out.println(setStatus(DRAW));
-        Assert.assertEquals(3, getNumber(DRAW));
+        Assert.assertEquals(Status.THIRD.getStatusValue(), setStatus(DRAW));
     }
 
     @Test
     public void getWinnerX() {
         System.out.println(setStatus(WINNER_IS_X));
-        Assert.assertEquals(4, getNumber(WINNER_IS_X));
+        Assert.assertEquals(Status.FOURTH.getStatusValue(), setStatus(WINNER_IS_X));
     }
 
     @Test
     public void getWinnerO() {
         System.out.println(setStatus(WINNER_IS_O));
-        Assert.assertEquals(5, getNumber(WINNER_IS_O));
+        Assert.assertEquals(Status.FIFTH.getStatusValue(), setStatus(WINNER_IS_O));
     }
 
     @Test
     public void getXContinues() {
         System.out.println(setStatus(X_CONTINUES));
-        Assert.assertEquals(6, getNumber(X_CONTINUES));
+        Assert.assertEquals(Status.SIXTH.getStatusValue(), setStatus(X_CONTINUES));
     }
 
     @Test
     public void getOContinues() {
         System.out.println(setStatus(O_CONTINUES));
-        Assert.assertEquals(7, getNumber(O_CONTINUES));
+        Assert.assertEquals(Status.SEVENTH.getStatusValue(), setStatus(O_CONTINUES));
     }
 
     @AfterClass
